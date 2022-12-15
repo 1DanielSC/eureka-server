aff45a214dbbc10e400988a5373d08967fa1c750
1danielsc <danielscolao@gmail.com>
2022-12-14 21:31:01 -0300

    feat: zipkin

--- src/main/java/com/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,9 @@ package com.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,11 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
